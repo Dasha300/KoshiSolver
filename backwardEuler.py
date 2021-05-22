0da5443420dcf3a8579ae3bf8f841c6156a59cf2
Dasha Denisova <daryalich@mail.ru>
2021-05-22 11:43:30 +0800

    corrected backward Euler method

--- backwardEuler.py
+++ backwardEuler.py
@@ -5,9 +5,9 @@ def backward_euler(f, h, y0, xn, x0):
     x, y, n = creator_vectors_and_n.create_x_y_n(xn, x0, h, y0)
     i = 0
     while i < n:
-        z = y[i] + h * f(x[i], y[i])
         x[i + 1] = x[i] + h
-        y[i + 1] = y[i] + h * f(x[i + 1], z)
+        z = y[i] + h * f(x[i], y[i])
+        y[i + 1] = y[i] + h/2*(f(x[i], y[i])+f(x[i + 1], z))
         if y[i+1] > 200000:
             break
         i += 1
