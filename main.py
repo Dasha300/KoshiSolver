a3e2850b1729433414cb107443d93e27babbd2d0
Dasha Denisova <daryalich@mail.ru>
2021-06-08 20:53:11 +0800

    exact solution

--- main.py
+++ main.py
@@ -19,6 +19,5 @@ x4, y4 = rungeKutta.rungeKutta(f=f, h=0.1, y0=0, xn=1, x0=0)
 fig = plt.figure()
 y5 = [1 / 4 * (5 * np.exp(2 * i) - 5 - 2 * i) for i in x2]
 l1, l2, l3, l4, l5, l6 = plt.plot(x, y, x1, y1, x2, y2, x3, y3, x4, y4, x, y5)
-fig.legend((l1, l2, l3, l4, l5, l6),
-           ('Euler', 'Backward Euler', 'Midpoint Euler', 'Adams-Bashforth Euler', 'Runge Kutta', 'Exact Solution'))
+fig.legend((l1, l2, l3, l4, l5, l6),('Euler', 'Backward Euler', 'Midpoint Euler', 'Adams-Bashforth Euler', 'Runge Kutta', 'Exact Solution'))
 plt.show()
